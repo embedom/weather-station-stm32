6bbb2b1349983cbe2ad47c1043ec20648f595c85
embedom <dominik.apps@proton.me>
2026-04-09 22:49:56 +0200

    Add stm32F7 hal driver as submodule, adjust drivers cmake file

--- dep-drivers-stm32.cmake
+++ dep-drivers-stm32.cmake
@@ -1,31 +1,31 @@
 ########################### STM32 drivers config ##############################
-set(STM32F7xx_HAL_DIR_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver)
+set(STM32F7xx_HAL_DIR_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/STM32F7xx_HAL)
 
 target_include_directories(${PROJECT_NAME} PUBLIC
     ${CMAKE_SOURCE_DIR}/Drivers/CMSIS/Include
     ${CMAKE_SOURCE_DIR}/Drivers/CMSIS/Device/ST/STM32F7xx/Include
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Inc
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy
+    ${STM32F7xx_HAL_DIR_PATH}/Inc
+    ${STM32F7xx_HAL_DIR_PATH}/Inc/Legacy
 )
 
 set(HAL_SOURCES
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_cortex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_tim_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_flash_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_i2c_ex.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_exti.c
-    ${CMAKE_SOURCE_DIR}/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_eth.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_cortex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_tim.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_tim_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_uart.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_uart_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_rcc.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_rcc_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_flash.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_flash_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_gpio.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_dma.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_dma_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_pwr.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_pwr_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_i2c.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_i2c_ex.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_exti.c
+    ${STM32F7xx_HAL_DIR_PATH}/Src/stm32f7xx_hal_eth.c
 )
